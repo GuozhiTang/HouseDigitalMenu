61d08ac34974fe176742428fad0f5f323102656e
Guozhi Tang <tangguozhi53@gmail.com>
2024-06-12 23:40:12 -0700

    Testing for separated FE and BE deployment.

---
+++ .next/static/an018hxawglhWsiLIAHfM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e3b030ee79813b54.css","static/chunks/pages/index-d15e0e189d712d91.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
